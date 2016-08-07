f365803b53a98c11151a9ab9f4795401c7b6c5cd
Rob H <robhowells01@gmail.com>
2016-08-07 10:35:23 +0100

    added gruntfile and package.json

---
+++ gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function (grunt) {
+    var jsFiles = ['js/src/vendors/*.js', 'js/src/components/*.js', 'js/src/*.js', 'js/src/app.js' ];
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dev: {
+                files: {
+                    'css/style.css': 'scss/style.scss',
+                },
+                options: {
+                    outputStyle: 'expanded'
+                }
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'scss/style.scss',
+                },
+                options: {
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9']
+            },
+            single_file: {
+                src: 'css/style.css',
+                dest: 'css/style.min.css' 
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass:dev', 'autoprefixer']
+            },
+            js: {
+                files: jsFiles,
+                tasks: ['uglify:dev']
+            }
+        },
+        uglify: {
+            dev: {
+                option: {
+                    compress: false
+                },
+                files: {
+                    'js/dist/js.min.js': jsFiles
+                } 
+            },
+            dist: {
+                option: {
+                    compress: true
+                },
+                files: {
+                    'js/dist/js.min.js': jsFiles
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: '.'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', ['sass:dist', 'autoprefixer', 'uglify:dev']);
+
+};
